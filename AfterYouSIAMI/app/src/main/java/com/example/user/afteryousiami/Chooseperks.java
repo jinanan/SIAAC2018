cf826c6bbed503a7f48bf43f6570c407ddd1723a
jinan <tan_jin_an@hotmail.com>
2018-09-28 01:47:05 +0800

    Fixed UI problems
    - now UI items are dynamically added
    - not gonna finish all my work cause i wanna keep my job

--- AfterYouSIAMI/app/src/main/java/com/example/user/afteryousiami/Chooseperks.java
+++ AfterYouSIAMI/app/src/main/java/com/example/user/afteryousiami/Chooseperks.java
@@ -2,16 +2,29 @@ package com.example.user.afteryousiami;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Layout;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import android.content.Context;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.user.afteryousiami.DAO.DBConnect;
 import com.example.user.afteryousiami.objects.Perks;
@@ -20,23 +33,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Chooseperks extends Activity {
-    final Context context = this;
-    Spinner spinner;
-    ArrayAdapter<CharSequence> adapter;
-    DBConnect db = new DBConnect();
+    private final Context context = this;
+    private Spinner spinner;
+    private ArrayAdapter<CharSequence> adapter;
+    private DBConnect db = new DBConnect();
+    private List<Perks> perksList;
+    private TableLayout layout;
+
+    //colors to be used for the textview
+    private final String WHITE_COLOR = "#ffffff";
+    private final String BLUE_COLOR = "#0C4082";
+    private final String GOLD_COLOR = "#FCB130";
+    private final int HEIGHT = 550;
+    private final int WIDTH = 1100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooseperks);
-
         spinner = (Spinner) findViewById(R.id.spinner);
-
+        layout = (TableLayout) findViewById(R.id.tableLayout);
         new PerksAsyncTask().execute();
-
     }
 
-    private void createSpinnerObjects(List<Perks> perksList){
+    private void createSpinnerObjects(List<Perks> perksList) {
+        this.perksList = perksList;     //set the object to be accessed by the spinenr selected method
         List<String> array = new ArrayList<>();
         for (Perks p : perksList) {
             String category = p.getCategory();
@@ -48,29 +70,179 @@ public class Chooseperks extends Activity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, array);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-//        spinner.invalidate();         //refresh the spinner
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView adapter, View v, int i, long lng) {
+                cleanTable();        //refresh the table items
+                String category = adapter.getItemAtPosition(i).toString();
+                updateUI(category);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+        });
+    }
+
+    /***
+     * used to update the UI based on the selected category from the spinner
+     * @param selectedCategory
+     */
+    private void updateUI(String selectedCategory) {
+        List<Perks> selected = new ArrayList<>();
+
+        for (Perks p : perksList) {
+            if (selectedCategory.equals(p.getCategory())) {
+                selected.add(p);
+            }
+        }
+
+        //update the texts or more items where possible
+        for (int i = 0; i < selected.size(); i++) {
+            Perks p = selected.get(i);
+            TableRow headerRow = new TableRow(this);
+            TableRow pictureRow = new TableRow(this);
+            TableRow descriptionRow = new TableRow(this);
+            TableRow addToCartRow = new TableRow(this);
+
+            //setting layout params
+            headerRow.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            headerRow.setPadding(0, 0, 0, 20);
+            pictureRow.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            pictureRow.setPadding(0, 0, 0, 20);
+            descriptionRow.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            descriptionRow.setPadding(0, 0, 0, 20);
+            if (i < selected.size() - 1) {              //as long as i is still lesser than the last element, add the padding, else dont add when its the last
+                addToCartRow.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                addToCartRow.setPadding(0, 0, 0, 20);
+            }
+
+            //add data from the textviews to the tablerows
+            headerRow.addView(createTextView(p.getName(), true));
+            createImageView(p.getName(), pictureRow);
+            descriptionRow.addView(createTextView(p.getDescription(), false));
+            addToCartRow.addView(createCartButton(p.getName()));
+
+
+            //add to the layout
+            layout.addView(headerRow);
+            layout.addView(pictureRow);
+            layout.addView(descriptionRow);
+            layout.addView(addToCartRow);
+        }
+
+        //add the checkout button
+        TableRow checkOutRow = new TableRow(this);
+        checkOutRow.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        checkOutRow.addView(createCheckOutButton());
+        layout.addView(checkOutRow);
 
     }
 
-    public void addToCart(View v) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setCancelable(true);
-        builder.setTitle("SIAMI");
-        builder.setMessage("Item added to Cart");
-        builder.setPositiveButton("Ok",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                });
+    /***
+     * helper method to clean the table. do not clean the first 2 items because thats the header and the spinner
+     */
+    private void cleanTable() {
 
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        int childCount = layout.getChildCount();
+
+        for (int i = childCount - 1; i >= 2; i--) {
+            TableRow row = (TableRow) layout.getChildAt(i);
+            layout.removeView(row);
+        }
     }
 
-    public void nxtPageBtn(View v) {
-        Intent i = new Intent(Chooseperks.this, perks_summary.class);
-        startActivity(i);
+    /***
+     * creates an imageview based on the name of the thing
+     * @param name
+     * @return imageview object
+     */
+    private ImageView createImageView(String name, TableRow pictureRow) {
+        ImageView imageView = new ImageView(this);
+        pictureRow.addView(imageView);
+        imageView.getLayoutParams().height = HEIGHT;
+        imageView.getLayoutParams().width = WIDTH;
+        imageView.setImageResource(R.drawable.krisflyerlounge);
+        return imageView;
+    }
+
+    private Button createCartButton(final String name) {
+        //<Button
+        //                    android:id="@+id/btn_addToCart"
+        //                    android:layout_width="wrap_content"
+        //                    android:layout_height="wrap_content"
+        //                    android:background="@color/gold"
+        //                    android:onClick="addToCart"
+        //                    android:text="Add to Cart"
+        //                    android:textColor="@color/white" />
+
+        Button btn = new Button(this);
+        btn.setBackgroundColor(Color.parseColor(GOLD_COLOR));
+        btn.setText("Add to cart");
+        btn.setGravity(Gravity.CENTER_HORIZONTAL);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Chooseperks.this, "clicked on> " + name, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return btn;
+    }
+
+    /***
+     * creates the check out button
+     * @return button object
+     */
+    private Button createCheckOutButton() {
+        Button btn = new Button(this);
+        btn.setBackgroundColor(Color.parseColor(BLUE_COLOR));
+        btn.setText("Checkout");
+        btn.setTextColor(Color.parseColor(WHITE_COLOR));
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Chooseperks.this, perks_summary.class);
+                startActivity(i);
+            }
+        });
+
+        return btn;
+
+    }
+
+    /***
+     * Creates a textview to be shown to the user
+     * @param value what you want to show to the user
+     * @param isHeader true = header, false = description, different is just a slight difference in text size and text color
+     * @return textview object
+     */
+    private TextView createTextView(String value, boolean isHeader) {
+        TextView textView = new TextView(this);
+        textView.setLayoutParams(new TableRow.LayoutParams());
+        textView.setText(value);
+        textView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+        if (isHeader) {
+            textView.setTextColor(Color.parseColor(GOLD_COLOR));
+            textView.setTextSize(18);
+        } else {
+            textView.setTextColor(Color.parseColor(BLUE_COLOR));
+            textView.setTextSize(16);
+        }
+        textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
+
+
+        return textView;
+    }
+
+    private static void printDebug(String message) {
+        Log.d("Chooseperks.java", message);
+    }
+
+    private static void printError(Exception e) {
+        Log.e("Chooseperks.java", "error", e);
     }
 
 
@@ -93,7 +265,7 @@ public class Chooseperks extends Activity {
         }
 
         @Override
-        protected void onPostExecute(List<Perks>  result) {
+        protected void onPostExecute(List<Perks> result) {
             super.onPostExecute(result);
 
             createSpinnerObjects(result);
