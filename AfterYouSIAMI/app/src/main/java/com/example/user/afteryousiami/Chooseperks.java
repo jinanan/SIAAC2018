dbea7691b0894abd3027c0c7396c9827152eafe1
jinan <tan_jin_an@hotmail.com>
2018-09-27 10:57:43 +0800

    Added perks list category

--- AfterYouSIAMI/app/src/main/java/com/example/user/afteryousiami/Chooseperks.java
+++ AfterYouSIAMI/app/src/main/java/com/example/user/afteryousiami/Chooseperks.java
@@ -1,6 +1,8 @@
 package com.example.user.afteryousiami;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -11,22 +13,46 @@ import android.content.Context;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 
+import com.example.user.afteryousiami.DAO.DBConnect;
+import com.example.user.afteryousiami.objects.Perks;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Chooseperks extends Activity {
     final Context context = this;
     Spinner spinner;
     ArrayAdapter<CharSequence> adapter;
+    DBConnect db = new DBConnect();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooseperks);
 
-        spinner = (Spinner)findViewById(R.id.spinner);
-        adapter = ArrayAdapter.createFromResource(this,R.array.perks_list,android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        new PerksAsyncTask().execute();
+
     }
 
-    public void addToCart(View v){
+    private void createSpinnerObjects(List<Perks> perksList){
+        List<String> array = new ArrayList<>();
+        for (Perks p : perksList) {
+            String category = p.getCategory();
+            if (!array.contains(category)) {
+                array.add(p.getCategory());
+            }
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, array);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+//        spinner.invalidate();         //refresh the spinner
+
+    }
+
+    public void addToCart(View v) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setCancelable(true);
         builder.setTitle("SIAMI");
@@ -42,9 +68,39 @@ public class Chooseperks extends Activity {
         dialog.show();
     }
 
-    public void nxtPageBtn(View v){
-        Intent i =  new Intent(Chooseperks.this, perks_summary.class );
+    public void nxtPageBtn(View v) {
+        Intent i = new Intent(Chooseperks.this, perks_summary.class);
         startActivity(i);
     }
 
+
+    class PerksAsyncTask extends AsyncTask<Void, Void, List<Perks>> {
+        ProgressDialog pdLoading = new ProgressDialog(Chooseperks.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.show();
+        }
+
+        @Override
+        protected List<Perks> doInBackground(Void... params) {
+            List<Perks> perksList = db.retrievePerks();
+            return perksList;
+        }
+
+        @Override
+        protected void onPostExecute(List<Perks>  result) {
+            super.onPostExecute(result);
+
+            createSpinnerObjects(result);
+
+            //this method will be running on UI thread
+            pdLoading.dismiss();
+        }
+
+    }
 }
