09fa3009105478b1897128fc97c3800ce716debf
jinan <tan_jin_an@hotmail.com>
2018-09-29 12:33:08 +0800

    now when the program is complete, the app shuts down

--- AfterYouSIAMI/app/src/main/java/com/example/user/afteryousiami/perks_summary.java
+++ AfterYouSIAMI/app/src/main/java/com/example/user/afteryousiami/perks_summary.java
@@ -341,8 +341,12 @@ public class perks_summary extends Activity {
                         .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 Toast.makeText(perks_summary.this, "Program complete.", Toast.LENGTH_LONG).show();
-                                perks_summary.this.finish();
                                 notificationcall();     //invoke the notification
+
+                                Intent intent = new Intent(Intent.ACTION_MAIN);
+                                intent.addCategory(Intent.CATEGORY_HOME);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
                             }
                         });
 
